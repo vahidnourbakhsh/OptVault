ba66f98a752f7a60d5db71762eb86b1c7714cdbe
Vahid Nourbakhsh <Vahid.Nourbakhsh@albertsons.com>
2023-06-19 08:06:35 -0700

    replace get_constraint_status with get_slacks

--- examples/dea/dea.py
+++ examples/dea/dea.py
@@ -63,6 +63,8 @@ def _input_oriented_model():
     # Decision vars
     model.theta = Var(within=NonNegativeReals)
     model.lmbda = Var(model.Units, within=NonNegativeReals)
+    model.input_slack = Var(model.Inputs, within=NonNegativeReals)
+    model.output_slack = Var(model.Outputs, within=NonNegativeReals)
 
     # Objective
     def theta_rule(model):
@@ -70,27 +72,24 @@ def _input_oriented_model():
     model.efficiency = Objective(rule=theta_rule, sense=minimize)
 
     # Constraints
-    def input_rule_value(model, input):
-        value = sum(model.lmbda[unit] * model.invalues[input, unit] for unit in model.Units) - sum(model.theta * model.target[unit] * model.invalues[input, unit] for unit in model.Units)
-        return value
-
     def input_rule(model, input):
-        value = input_rule_value(model, input)
-        return inequality(body=value, upper=0)
+        value = (
+            sum(model.lmbda[unit] * model.invalues[input, unit] for unit in model.Units) 
+            - sum(model.theta * model.target[unit] * model.invalues[input, unit] for unit in model.Units)
+            + model.input_slack[input]
+        )
+        return value == 0
     model.input_cons = Constraint(model.Inputs, rule=input_rule)
 
 
-    def output_rule_value(model, output):
-        value = sum(model.lmbda[unit] * model.outvalues[output, unit] for unit in model.Units) - sum(model.target[unit] * model.outvalues[output, unit] for unit in model.Units)
-        return value
-
     def output_rule(model, output):
-        value = output_rule_value(model, output)
-        return inequality(body=value, lower=0)
+        value = (
+            sum(model.lmbda[unit] * model.outvalues[output, unit] for unit in model.Units) 
+            - sum(model.target[unit] * model.outvalues[output, unit] for unit in model.Units)
+            - model.output_slack[output]
+        )
+        return value == 0
     model.output_cons = Constraint(model.Outputs, rule=output_rule)
-
-    model.input_rule_value = input_rule_value
-    model.output_rule_value = output_rule_value
 
     return model
 
@@ -161,16 +160,12 @@ class DEAProgram:
     def _create_abstract_model(self, model_type="primal") -> AbstractModel:
         return self.MODELS[model_type]
 
-    def get_constraint_status(self, instance, data) -> pd.DataFrame:
-        lmbda_sol_values = [0] + [x.value for x in instance.lmbda.values()] 
-        theta_sol_values = instance.theta()
-
+    def get_slacks(self, instance, data):
         constraint_status = pd.DataFrame(columns=['slack'])
         for input_feature in data[None]["Inputs"][None]:
-            slack = round(eval(str(self.model.input_rule_value(instance, input_feature)), {"lmbda": lmbda_sol_values, "theta": theta_sol_values}), 2)
+            slack = instance.input_slack[input_feature]()
             constraint_status.loc[input_feature] = [slack]
         for output_feature in data[None]["Outputs"][None]:
-            slack = round(eval(str(self.model.output_rule_value(instance, output_feature)), {"lmbda": lmbda_sol_values, "theta": theta_sol_values}), 2)
+            slack = instance.output_slack[output_feature]()
             constraint_status.loc[output_feature] = [slack]
-
         return constraint_status
