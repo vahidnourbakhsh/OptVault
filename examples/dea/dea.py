cb67843f50087269ff7ed896ccf7c7e4c333e11c
Vahid Nourbakhsh <vnourba@gmail.com>
2023-07-03 23:04:30 -0700

    format black

--- examples/dea/dea.py
+++ examples/dea/dea.py
@@ -1,6 +1,19 @@
-from pyomo.environ import AbstractModel, Set, Param, Var, Objective, Constraint, PositiveReals, NonNegativeReals, Binary, maximize, inequality, SolverFactory
+from pyomo.environ import (
+    AbstractModel,
+    Set,
+    Param,
+    Var,
+    Objective,
+    Constraint,
+    PositiveReals,
+    NonNegativeReals,
+    Binary,
+    maximize,
+    inequality,
+    SolverFactory,
+)
 
-TOLERANCE = 0.01 # feasibility tolerance for the normalization constraint below
+TOLERANCE = 0.01  # feasibility tolerance for the normalization constraint below
 
 model = AbstractModel()
 # Sets
@@ -17,18 +30,37 @@ model.target = Param(model.Units, within=Binary)
 model.u = Var(model.Outputs, within=NonNegativeReals)
 model.v = Var(model.Inputs, within=NonNegativeReals)
 
+
 # Objective
 def efficiency_rule(model):
-    return sum(model.outvalues[j, unit]*model.target[unit]*model.u[j] for unit in model.Units for j in model.Outputs)
+    return sum(
+        model.outvalues[j, unit] * model.target[unit] * model.u[j]
+        for unit in model.Units
+        for j in model.Outputs
+    )
+
+
 model.efficiency = Objective(rule=efficiency_rule, sense=maximize)
+
 
 # Constraints
 def ratio_rule(model, unit):
-    value = sum(model.outvalues[j, unit]*model.u[j] for j in model.Outputs) - sum(model.invalues[i, unit]*model.v[i] for i in model.Inputs)
+    value = sum(model.outvalues[j, unit] * model.u[j] for j in model.Outputs) - sum(
+        model.invalues[i, unit] * model.v[i] for i in model.Inputs
+    )
     return inequality(body=value, upper=0)
+
+
 model.ratio = Constraint(model.Units, rule=ratio_rule)
 
+
 def normalization_rule(model):
-    value = sum(model.invalues[i, unit]*model.target[unit]*model.v[i] for unit in model.Units for i in model.Inputs)
-    return inequality(body=value, lower=1-TOLERANCE, upper=1 + TOLERANCE)
+    value = sum(
+        model.invalues[i, unit] * model.target[unit] * model.v[i]
+        for unit in model.Units
+        for i in model.Inputs
+    )
+    return inequality(body=value, lower=1 - TOLERANCE, upper=1 + TOLERANCE)
+
+
 model.normalization = Constraint(rule=normalization_rule)
